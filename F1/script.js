0f8080ca96404dc1d585d30140b7e4b1118f0135
Aarav <22cs038@charusat.edu.in>
2023-10-11 10:37:14 +0530

    js1

--- F1/script.js
+++ F1/script.js
@@ -1,1 +1,21 @@
-//This is js part of my code
+// Extend the base functionality of JavaScript
+Array.prototype.last = function () {
+  return this[this.length - 1];
+};
+
+// A sinus function that acceps degrees instead of radians
+Math.sinus = function (degree) {
+  return Math.sin((degree / 180) * Math.PI);
+};
+
+// Game data
+let phase = "waiting"; // waiting | stretching | turning | walking | transitioning | falling
+let lastTimestamp; // The timestamp of the previous requestAnimationFrame cycle
+
+let heroX; // Changes when moving forward
+let heroY; // Only changes when falling
+let sceneOffset; // Moves the whole game
+
+let platforms = [];
+let sticks = [];
+let trees = [];
