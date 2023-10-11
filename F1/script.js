cd1e31baa80859664460cc51ae750644d95ee481
Aarav <22cs038@charusat.edu.in>
2023-10-11 10:37:46 +0530

    js

--- F1/script.js
+++ F1/script.js
@@ -19,3 +19,139 @@ let sceneOffset; // Moves the whole game
 let platforms = [];
 let sticks = [];
 let trees = [];
+
+// Todo: Save high score to localStorage (?)
+
+let score = 0;
+
+// Configuration
+const canvasWidth = 375;
+const canvasHeight = 375;
+const platformHeight = 100;
+const heroDistanceFromEdge = 10; // While waiting
+const paddingX = 100; // The waiting position of the hero in from the original canvas size
+const perfectAreaSize = 10;
+
+// The background moves slower than the hero
+const backgroundSpeedMultiplier = 0.2;
+
+const hill1BaseHeight = 100;
+const hill1Amplitude = 10;
+const hill1Stretch = 1;
+const hill2BaseHeight = 70;
+const hill2Amplitude = 20;
+const hill2Stretch = 0.5;
+
+const stretchingSpeed = 4; // Milliseconds it takes to draw a pixel
+const turningSpeed = 4; // Milliseconds it takes to turn a degree
+const walkingSpeed = 4;
+const transitioningSpeed = 2;
+const fallingSpeed = 2;
+
+const heroWidth = 17; // 24
+const heroHeight = 30; // 40
+
+const canvas = document.getElementById("game");
+canvas.width = window.innerWidth; // Make the Canvas full screen
+canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext("2d");
+
+const introductionElement = document.getElementById("introduction");
+const perfectElement = document.getElementById("perfect");
+const restartButton = document.getElementById("restart");
+const scoreElement = document.getElementById("score");
+
+// Initialize layout
+resetGame();
+
+// Resets game variables and layouts but does not start the game (game starts on keypress)
+function resetGame() {
+  // Reset game progress
+  phase = "waiting";
+  lastTimestamp = undefined;
+  sceneOffset = 0;
+  score = 0;
+
+  introductionElement.style.opacity = 1;
+  perfectElement.style.opacity = 0;
+  restartButton.style.display = "none";
+  scoreElement.innerText = score;
+
+  // The first platform is always the same
+  // x + w has to match paddingX
+  platforms = [{ x: 50, w: 50 }];
+  generatePlatform();
+  generatePlatform();
+  generatePlatform();
+  generatePlatform();
+
+  sticks = [{ x: platforms[0].x + platforms[0].w, length: 0, rotation: 0 }];
+
+  trees = [];
+  generateTree();
+  generateTree();
+  generateTree();
+  generateTree();
+  generateTree();
+  generateTree();
+  generateTree();
+  generateTree();
+  generateTree();
+  generateTree();
+
+  heroX = platforms[0].x + platforms[0].w - heroDistanceFromEdge;
+  heroY = 0;
+
+  draw();
+}
+
+function generateTree() {
+  const minimumGap = 30;
+  const maximumGap = 150;
+
+  // X coordinate of the right edge of the furthest tree
+  const lastTree = trees[trees.length - 1];
+  let furthestX = lastTree ? lastTree.x : 0;
+
+  const x =
+    furthestX +
+    minimumGap +
+    Math.floor(Math.random() * (maximumGap - minimumGap));
+
+  const treeColors = ["#6D8821", "#8FAC34", "#98B333"];
+  const color = treeColors[Math.floor(Math.random() * 3)];
+
+  trees.push({ x, color });
+}
+
+function generatePlatform() {
+  const minimumGap = 40;
+  const maximumGap = 200;
+  const minimumWidth = 20;
+  const maximumWidth = 100;
+
+  // X coordinate of the right edge of the furthest platform
+  const lastPlatform = platforms[platforms.length - 1];
+  let furthestX = lastPlatform.x + lastPlatform.w;
+
+  const x =
+    furthestX +
+    minimumGap +
+    Math.floor(Math.random() * (maximumGap - minimumGap));
+  const w =
+    minimumWidth + Math.floor(Math.random() * (maximumWidth - minimumWidth));
+
+  platforms.push({ x, w });
+}
+
+resetGame();
+
+// If space was pressed restart the game
+window.addEventListener("keydown", function (event) {
+  if (event.key == " ") {
+    event.preventDefault();
+    resetGame();
+    return;
+  }
+});
